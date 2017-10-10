cf367308fecae5d9e2b2b088621d2547d8e99f77
vinicius <vinicius@debian>
2017-10-09 21:58:36 -0300

    ch04

---
+++ ch04/simple_todo.ex
@@ -0,0 +1,15 @@
+defmodule TodoList do
+
+  alias MultiDict
+
+  def new, do: MultiDict.new
+
+  def add_entry(todo_list, date, title) do
+    MultiDict.add(todo_list, date, title)
+  end
+
+  def entries(todo_list, date) do
+    MultiDict.get(todo_list, date)
+  end
+
+end
