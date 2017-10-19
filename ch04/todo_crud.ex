0dc887fcd6d5d3854345ce6c98e720bc30a81987
Vinicius Chagas <vchagas@xerpa.com.br>
2017-10-19 19:44:17 -0200

    FIXME todo_crud delete and maybe others

--- ch04/todo_crud.ex
+++ ch04/todo_crud.ex
@@ -1,3 +1,66 @@
 defmodule TodoList do
-  
+  defstruct auto_id: 1, entries: Map.new
+
+  def new(entries \\ []) do
+    Enum.reduce(
+      entries,
+      %TodoList {},
+      &add_entry(&2, &1))
+  end
+
+  # def new, do: %TodoList{}
+
+  def entries(%TodoList{entries: entries}, date) do
+    entries
+    |> Stream.filter(fn({_, entry}) ->
+      entry.date == date
+    end)
+    |> Enum.map(fn({_, entry}) ->
+      entry
+    end)
+  end
+
+  def add_entry(
+    %TodoList{entries: entries, auto_id: auto_id} = todolist,
+    entry
+  ) do
+    entry = Map.put(entry, :id, auto_id)
+    new_entries = Map.put(entries, auto_id, entry)
+
+    %TodoList{todolist |
+              entries: new_entries,
+              auto_id: auto_id + 1
+    }
+  end
+
+  def update_entry(todo_list, %{} = new_entry) do
+    update_entry(todo_list, new_entry.id, fn(_) -> new_entry end)
+  end
+
+  def update_entry(
+    %TodoList{entries: entries} = todo_list,
+    entry_id,
+    update_fun
+  ) do
+    case entries[entry_id] do
+      nil -> todo_list
+
+      old_entry ->
+        new_entry = update_fun.(old_entry)
+        new_entries = Map.put(entries, new_entry.id, new_entry)
+        %TodoList{todo_list | entries: new_entries}
+    end
+  end
+
+  def delete_entry(
+    %TodoList{entries: entries} = todo_list,
+    entry_id
+  ) do
+    case entries[entry_id] do
+      nil -> todo_list
+
+      _ ->
+        Map.drop(todo_list, entry_id)
+    end
+  end
 end
